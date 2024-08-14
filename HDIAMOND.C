7f3a01a19da37a8ebda43f2142c18c43e39e727e
mariselvam08 <marimass961@gmail.com>
2024-08-14 15:31:28 +0530

    Add files via upload

---
+++ HDIAMOND.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int i,j,s,n;
+clrscr();
+printf("Enter the number");
+scanf("%d",&n);
+for(i=1;i<=n-1;i++){
+for(s=2;s<=n-i;s++){
+printf(" ");
+}
+for(j=2;j<=2*i;j++){
+if(j==2||j==2*i)
+printf("*");
+else
+printf(" ");
+}
+printf("\n");
+}
+for(i=n-2;i>=1;i--){
+for(s=1;s<n-i;s++){
+printf(" ");
+}
+for(j=2;j<=2*i;j++){
+if(j==2||j==2*i){
+printf("*");
+}
+else
+printf(" ");
+}
+printf("\n");
+}
+getch();
+}
